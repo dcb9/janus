a6a706b2eb49129b797a71204e068b731c4d7f41
dcb9 <bob@phpor.me>
2018-08-09 11:18:26 +0800

    Add testnet truffle config

--- playground/truffle.js
+++ playground/truffle.js
@@ -8,6 +8,13 @@ module.exports = {
       network_id: "*",
       from: "0x7926223070547d2d15b2ef5e7383e541c338ffe9",
       gasPrice: "0x64"
+    },
+    testnet: {
+      host: "hk1.s.qtum.org",
+      port: 23889,
+      network_id: "*",
+      from: "0x7926223070547d2d15b2ef5e7383e541c338ffe9",
+      gasPrice: "0x64"
     }
   }
 };
